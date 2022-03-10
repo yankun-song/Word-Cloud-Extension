99595c517f0e89aa1df127ccf5a66f2007fb1874
Yankun Song <yankun.l.song@gmail.com>
2022-03-10 10:21:19 -0600

    done freq

--- popup.js
+++ popup.js
@@ -8,34 +8,55 @@ chrome.storage.sync.get("color", ({ color }) => {
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+let changeColor = document.getElementById("changeColor");
 
   console.log(tab.url);
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: contentGetter,
+    function: wordRank,
   });
 });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// }
 
-function contentGetter() {
-  var bodyScripts = document.querySelectorAll("body script");
-  for (var i = 0; i < bodyScripts.length; i++) {
-    bodyScripts[i].remove();
+function wordRank() {
+  function contentGetter() {
+    var bodyScripts = document.querySelectorAll("body script");
+    for (var i = 0; i < bodyScripts.length; i++) {
+      bodyScripts[i].remove();
+    }
+    var str = document.body.innerText;
+    return str;
   }
-  var str = document.body.textContent;
-  console.log("hello Yankun!", str);
-  window.open(
-    // "/Users/yankunsong/Desktop/wordcloud/chart.html",
-    "/Users/yankunsong/Desktop/wordcloud/chart.html",
-    "CNN_WindowName"
-  );
 
-  //document.body.innerHTML = "<pre>" + str + "</pre>";
+  function contentToArr(content) {
+    const arr = [];
+    paragraphs = content.split("\n").filter((el) => el.length > 15);
+    for (let para of paragraphs) {
+      para = para.replace(/[^\w\s]|_/g, "");
+      arr.push(...para.split(" ").filter((el) => el.length > 4));
+    }
+    return arr;
+  }
+
+  function sortArrayByFreq(words) {
+    const counter = Object.create(null);
+    words.forEach(function (word) {
+      counter[word] = (counter[word] || 0) + 1;
+    });
+    const blacklist = ["their", "about"];
+    blacklist.forEach((el) => (counter[el] = 0));
+    let entries = Object.entries(counter);
+    let sorted = entries.sort((a, b) => -a[1] + b[1]);
+    return sorted.slice(0, 5);
+  }
+  const str = contentGetter();
+  const words = contentToArr(str);
+  const word5 = sortArrayByFreq(words);
 }
--- testAPI.js
+++ testAPI.js
@@ -1,24 +1,36 @@
-var request = require("request");
-const fs = require("fs");
+let toDo = `6 min read
+Cops Framing Suspects: My Take on Why Every Traffic Stop Is Terrifying
+A Florida officer getting 12 years in prison for planting drugs on suspects triggered me and probably you too.
+A police officer makes a traffic stop. Photo: Getty Images
 
-var options = {
-  method: "POST",
-  url: "https://quickchart.io/wordcloud",
-  headers: {},
-  body: {
-    format: "png",
-    width: 1000,
-    height: 1000,
-    fontFamily: "sans-serif",
-    fontScale: 15,
-    scale: "linear",
-    text: "<churchill's full speech churchill churchill churchill churchill churchill>",
-  },
-  json: true,
-};
+B
+eing pulled over by the police scares me to death. Long before Philando Castille, Sandra Bland, and Walter Scott — all Black people who wound up dying after a traffic stop-— became national news, a cop switching lanes to drive behind me used to make my heart skip a beat. But being stopped is just the tip of the iceberg. Black folks often get framed during these stops. And as I was reading the recent story of a cop that, this past July, was sentenced to 12 years in prison for planting drugs on motorists, I was triggered. The story of this former officer made me remember a traffic stop in Virginia where I thought I too might be framed for a crime I didn’t commit.
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
-  //fs.appendFileSync("word.png", response);
-  console.log(body);
-});
+In 2012, I was fresh out of law school and living with my cousin in Fredericksburg, Virginia. The city is about 50 miles south of northern Virginia, the “V” part of the DMV, and it’s also north of Richmond, a much more populated area. Fredericksburg is the kind of place a Black person drives through, not stops in.
+`;
+
+function contentToArr(content) {
+
+  const arr = [];
+  paragraphs = content.split("\n").filter((el) => el.length > 15);
+  for (let para of paragraphs) {
+    para = para.replace(/[^\w\s]|_/g, "");
+    arr.push(...para.split(" ").filter((el) => el.length > 4));
+  }
+  return arr;
+}
+
+const words = contentToArr(toDo);
+
+
+function sortArrayByFreq(words) {
+  const counter = Object.create(null);
+  words.forEach(function (word) {
+    counter[word] = (counter[word] || 0) + 1;
+  });
+  let entries = Object.entries(counter);
+  let sorted = entries.sort((a, b) => -a[1] +b[1]);
+  return sorted.slice(0,5);
+}
+
+console.log(sortArrayByFreq(words));
